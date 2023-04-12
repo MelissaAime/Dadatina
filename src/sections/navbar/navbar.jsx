909e775386ae9ec5872e16a65b71ab68c5e6e13f
Melissa Aime <aimemelissa1@gmail.com>
2023-04-12 17:20:16 -0300

    navbar first part

--- src/sections/navbar/navbar.jsx
+++ src/sections/navbar/navbar.jsx
@@ -1,8 +1,37 @@
 export const Navbar = () => {
+
+    const items = [
+        {
+            'id': 1,
+            'title': 'Inicio',
+        },
+        {
+            'id': 2,
+            'title': 'Sobre mi',
+        },
+        {
+            'id': 3,
+            'title': 'Productos',
+        },
+        {
+            'id': 4,
+            'title': 'Libro',
+        },
+        {
+            'id': 5,
+            'title': 'Redes',
+        }
+    ]
+
     return(
         <header>
-            <ul>
-
+            <img src="logo" alt="logo" className="navbar-logo"/>
+            <ul className="navbar">
+            {items.map(({id, title}) => (
+                <li key={id}>
+                    <a href="!#" className="navbar-items">{title}</a>
+                </li>
+            ))}
             </ul>
         </header>
     )
