61d71675fb68bfdb045b07641d3595518f0d9311
Melissa Aime <aimemelissa1@gmail.com>
2023-04-12 17:31:01 -0300

    new cards style and code

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,15 +1,11 @@
 export const Cards = ({title, text, image, alt}) => {
     return (
-        <div className="container">
-            <div className='cards'>
-                <div className="cards-img">
-                    <img  src={image} alt={alt}/>
-                </div>
-                <div className="cards-details">
-                    <h2>{title}</h2>
-                    <p>{text}</p>
-                </div>
-            </div>
-        </div>
+        <figure className="card">
+            <img src={image} alt={alt}/>
+            <figcaption>
+                <h2>{title}</h2>
+                <p>{text}</p>
+            </figcaption>
+        </figure>
     );
 }
