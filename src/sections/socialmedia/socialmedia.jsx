a0707a2720908ea74cdbbeafb9328d4c2837d288
Melissa Aime <aimemelissa1@gmail.com>
2023-05-10 14:24:20 -0300

    add social media

--- src/sections/socialmedia/socialmedia.jsx
+++ src/sections/socialmedia/socialmedia.jsx
@@ -1,6 +1,7 @@
 import { MyNavigation } from "../../components/Navigation/navigation";
 import { HeroImage } from "../../components/heroImage/heroImage";
 
+
 export const SocialMedia = () => {
 
     const imageLink = 'https://via.placeholder.com/300';
@@ -9,6 +10,12 @@ export const SocialMedia = () => {
      <>
         <HeroImage imageLink={imageLink} />
         <MyNavigation />
+
+        <div className="social-media">
+            <div data-mc-src="19902743-aa32-49ba-a939-6b338c64f109#instagram"></div>
+            <div data-mc-src="59dad293-f5c1-483f-a1a0-c1aa09306eb5#youtube"></div>
+        </div>
+        
      </>
     )
 }
