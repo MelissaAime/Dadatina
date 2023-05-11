321380bfe7ebafbe5f8421846bd69946b4d9155f
Melissa Aime <aimemelissa1@gmail.com>
2023-05-11 17:42:58 -0300

    socialmedia

--- src/sections/socialmedia/socialmedia.jsx
+++ src/sections/socialmedia/socialmedia.jsx
@@ -9,10 +9,11 @@ export const SocialMedia = () => {
         <HeroImage imageLink={imageLink} />
         <MyNavigation />
 
-        <div className="social-media">
+        <div className="social-media shapedividers_com-3121">
             <div data-mc-src="19902743-aa32-49ba-a939-6b338c64f109#instagram"></div>
-            <div data-mc-src="59dad293-f5c1-483f-a1a0-c1aa09306eb5#youtube"></div>
+            <div className="social-media-youtube" data-mc-src="59dad293-f5c1-483f-a1a0-c1aa09306eb5#youtube"></div>
         </div>
+        
         
      </>
     )
