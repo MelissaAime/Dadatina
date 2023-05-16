ad6bde8e1b4481127144beeb713a7e36f764c0bd
Melissa Aime <aimemelissa1@gmail.com>
2023-05-16 18:02:59 -0300

    book section

--- src/sections/book/book.jsx
+++ src/sections/book/book.jsx
@@ -4,11 +4,15 @@ import { HeroImage } from "../../components/heroImage/heroImage";
 export const MyBook = () => {
 
     const imageLink = 'https://via.placeholder.com/300';
-
+    const bookImage = 'https://pedidosfarma.vtexassets.com/arquivos/ids/182814-1200-auto?v=637565885549970000&width=1200&height=auto&aspect=true';
     return(
      <>
         <HeroImage imageLink={imageLink} />
         <MyNavigation />
+
+        <div className="book">
+            <img src={bookImage} alt="book" className="book-image" />
+        </div>
      </>
     )
 }
