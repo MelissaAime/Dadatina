104ce57e579c619cfaa2d5591db6511a3cc480b6
Melissa Aime <aimemelissa1@gmail.com>
2023-05-16 18:26:57 -0300

    social media responsive

--- src/sections/socialmedia/socialmedia.jsx
+++ src/sections/socialmedia/socialmedia.jsx
@@ -14,19 +14,19 @@ export const SocialMedia = () => {
             <div className="social-media-youtube" data-mc-src="59dad293-f5c1-483f-a1a0-c1aa09306eb5#youtube"></div>
             
             <div className="social-media-tiktok">
-                <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7232787936599674117" data-video-id="7232787936599674117" style={{"max-width": "405px", "min-width": "225px"}} > 
+                <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7232787936599674117" data-video-id="7232787936599674117" > 
                     <section> 
                         <a target="_blank" rel="noreferrer" title="@soydadatina" href="https://www.tiktok.com/@soydadatina?refer=embed">@soydadatina</a> 
                         <p>😜</p> 
                         <a target="_blank" rel="noreferrer" title="♬ THEY ARE GONNA KNOW - Heliqs" href="https://www.tiktok.com/music/THEY-ARE-GONNA-KNOW-7046738744150919169?refer=embed">♬ THEY ARE GONNA KNOW - Heliqs</a> 
                     </section> 
                 </blockquote> 
-                <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7211861995908369669" data-video-id="7211861995908369669" style={{"max-width": "405px", "min-width": "225px"}} > 
+                <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7211861995908369669" data-video-id="7211861995908369669" > 
                     <section> 
                         <a target="_blank" rel="noreferrer" title="@soydadatina" href="https://www.tiktok.com/@soydadatina?refer=embed">@soydadatina</a> Cómo recuperé mis uñas despues de abusar del semi.  
                     </section> 
                 </blockquote>
-                <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7225009321480899867" data-video-id="7225009321480899867" style={{"max-width": "405px", "min-width": "225px"}} >
+                <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@soydadatina/video/7225009321480899867" data-video-id="7225009321480899867" >
                     <section> 
                         <a target="_blank" rel="noreferrer" title="@soydadatina" href="https://www.tiktok.com/@soydadatina?refer=embed">@soydadatina</a> 
                         <a title="grwm" target="_blank" rel="noreferrer" href="https://www.tiktok.com/tag/grwm?refer=embed">#GRWM</a> 
