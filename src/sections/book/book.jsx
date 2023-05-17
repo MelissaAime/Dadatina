fe334e93a94e744ee1faa893b7d8a0ca60e36130
Melissa Aime <aimemelissa1@gmail.com>
2023-05-17 16:29:22 -0300

    book section

--- src/sections/book/book.jsx
+++ src/sections/book/book.jsx
@@ -13,6 +13,7 @@ export const MyBook = () => {
 
         <div className="book">
             <img src={bookImage} alt="book" className="book-image" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat laudantium voluptates quisquam, ipsum harum optio quae ipsa, ipsam consectetur ducimus dolorem quos molestiae repudiandae vitae! Repellat eum dolores perspiciatis saepe? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quo quaerat quam perferendis deleniti accusantium laboriosam quasi sequi ratione nihil, veniam illum cum aspernatur. Rerum suscipit voluptatum quidem perferendis nobis.</p>
         </div>
      </>
     )
