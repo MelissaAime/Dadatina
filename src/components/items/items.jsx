347621e20430f6f5c28ac63719b7a45514123d11
Melissa Aime <aimemelissa1@gmail.com>
2023-05-18 19:17:13 -0300

    tooltip on footer

--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -1,7 +1,36 @@
-export const Items = ({icon, href}) => {
+import React, { useRef, useEffect } from 'react';
+import {Tooltip} from "react-tooltip";
+
+export const Items = ({icon, href, tooltipContent}) => {
+    const tooltipRef = useRef(null);
+
+    useEffect(() => {
+      if (tooltipRef.current) {
+        tooltipRef.current.show();
+      }
+    }, []);
+
     return(
         <>
-            <a className="items-button" href={href} target="_blank" rel="noreferrer">{icon}</a>
+            <a 
+                className="items-button" 
+                href={href} 
+                target="_blank" rel="noreferrer" 
+                data-tooltip-id="myTooltip"
+                data-tooltip-content={tooltipContent}
+            >
+                {icon}
+            </a>
+
+            <Tooltip 
+                className="tooltip" 
+                id="myTooltip" 
+                ref={tooltipRef}
+                place="top"
+                effect="solid"
+            >
+                {tooltipContent}
+            </Tooltip>
         </>
     )
 }
--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -7,19 +7,38 @@ export const Footer = () => {
     const iconTiktok = <BsTiktok/>
     const iconTwitch = <BsTwitch/>
     const iconTwitter = <BsTwitter/>
+    const tooltipContentInstagramTikTok = "@soydadatina"
+    const tooltipContentTwitchTwitter = "dadatina"
+
     return(
         <div className="footer-container">
             <div className="footer-element"> 
-                <Items href={"https://www.instagram.com"} icon={iconInstagram}/>
+                <Items 
+                    href={"https://www.instagram.com"} 
+                    icon={iconInstagram} 
+                    tooltipContent={tooltipContentInstagramTikTok}
+                />
             </div>
             <div className="footer-element">
-                <Items href={"https://www.tiktok.com"} icon={iconTiktok}/>
+                <Items 
+                    href={"https://www.tiktok.com"} 
+                    icon={iconTiktok} 
+                    tooltipContent={tooltipContentInstagramTikTok}
+                />
             </div>
             <div className="footer-element">
-                <Items href={"https://www.twitch.com"} icon={iconTwitch}/>
+                <Items 
+                    href={"https://www.twitch.com"} 
+                    icon={iconTwitch}
+                    tooltipContent={tooltipContentTwitchTwitter}
+                />
             </div>
             <div className="footer-element">
-                <Items href={"https://www.twitter.com"} icon={iconTwitter}/>
+                <Items 
+                    href={"https://www.twitter.com"} 
+                    icon={iconTwitter} 
+                    tooltipContent={tooltipContentTwitchTwitter}
+                />
             </div>
         </div>
     )
