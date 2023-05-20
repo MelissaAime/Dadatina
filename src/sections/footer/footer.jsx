aaee2b149a755b97c104f0a88a2a1d4647e4cb9b
Melissa Aime <aimemelissa1@gmail.com>
2023-05-20 11:27:07 -0300

    footer styles

--- src/sections/footer/footer.jsx
+++ src/sections/footer/footer.jsx
@@ -1,5 +1,5 @@
 import { Items } from "../../components/items/items";
-import { BsInstagram, BsTiktok, BsTwitch, BsTwitter} from 'react-icons/bs';
+import { BsInstagram, BsTiktok, BsTwitch, BsTwitter, BsYoutube, BsFacebook, BsSpotify, BsChatRightTextFill} from 'react-icons/bs';
 
 export const Footer = () => {
 
@@ -7,38 +7,56 @@ export const Footer = () => {
     const iconTiktok = <BsTiktok/>
     const iconTwitch = <BsTwitch/>
     const iconTwitter = <BsTwitter/>
+    const iconYouTube = <BsYoutube/>
     const tooltipContentInstagramTikTok = "@soydadatina"
     const tooltipContentTwitchTwitter = "dadatina"
 
     return(
-        <div className="footer-container">
-            <div className="footer-element"> 
-                <Items 
-                    href={"https://www.instagram.com"} 
-                    icon={iconInstagram} 
-                    tooltipContent={tooltipContentInstagramTikTok}
-                />
+        <div className="footer">
+            <div>
+                <h4>Redes sociales:</h4>
             </div>
-            <div className="footer-element">
-                <Items 
-                    href={"https://www.tiktok.com"} 
-                    icon={iconTiktok} 
-                    tooltipContent={tooltipContentInstagramTikTok}
-                />
+            <div className="footer-redes">
+                <div className="footer-redes-element"> 
+                    <Items 
+                        href={"https://www.instagram.com"} 
+                        icon={iconInstagram} 
+                        tooltipContent={tooltipContentInstagramTikTok}
+                    />
+                </div>
+                <div className="footer-redes-element">
+                    <Items 
+                        href={"https://www.tiktok.com"} 
+                        icon={iconTiktok} 
+                        tooltipContent={tooltipContentInstagramTikTok}
+                    />
+                </div>
+                <div className="footer-redes-element">
+                    <Items 
+                        href={"https://www.twitch.com"} 
+                        icon={iconTwitch}
+                        tooltipContent={tooltipContentTwitchTwitter}
+                    />
+                </div>
+                <div className="footer-redes-element">
+                    <Items 
+                        href={"https://www.twitter.com"} 
+                        icon={iconTwitter} 
+                        tooltipContent={tooltipContentTwitchTwitter}
+                    />
+                </div>
+                <div className="footer-redes-element">
+                    <Items 
+                        href={"https://www.youtube.com"} 
+                        icon={iconYouTube} 
+                        tooltipContent={tooltipContentTwitchTwitter}
+                    />
+                </div>
             </div>
-            <div className="footer-element">
-                <Items 
-                    href={"https://www.twitch.com"} 
-                    icon={iconTwitch}
-                    tooltipContent={tooltipContentTwitchTwitter}
-                />
-            </div>
-            <div className="footer-element">
-                <Items 
-                    href={"https://www.twitter.com"} 
-                    icon={iconTwitter} 
-                    tooltipContent={tooltipContentTwitchTwitter}
-                />
+            <div className="footer-secondary">
+                <p> <BsSpotify/> Spotify</p>
+                <p> <BsChatRightTextFill/> Mail</p>
+                <p> <BsFacebook/> Facebook</p>
             </div>
         </div>
     )
